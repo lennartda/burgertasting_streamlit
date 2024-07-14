f0c5a64740714ce7b927a7d6b2a4fb197743112d
lennart <lennart.dangers@web.de>
2024-07-14 16:38:31 +0200

    initial commit burgerStreamlit

---
+++ test.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+from sqlalchemy import create_engine
+import env
+import os
+# Verbindung zur PostgreSQL-Datenbank herstellen
+def get_data():
+    database_url = env.DATABASE_URL
+    engine = create_engine(database_url)
+    query = "SELECT * FROM stg_burger_mart.dim_date"
+    df = pd.read_sql(query, engine)
+    return df
+
+# Titel des Dashboards
+st.title('Mein Streamlit Dashboard')
+
+# Daten abrufen und anzeigen
+df = get_data()
+print(df.head())
+st.write(df)
+
+
