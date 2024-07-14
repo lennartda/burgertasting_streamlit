4d56c2d39938dba82eef411cbb0bc5d95d84e8c8
lennart <lennart.dangers@web.de>
2024-07-14 17:02:25 +0200

    Replace streamlit secrets

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import env
 import os
 # Verbindung zur PostgreSQL-Datenbank herstellen
 def get_data():
-    database_url = env.DATABASE_URL
+    database_url = st.secrets["database_url"]
     engine = create_engine(database_url)
     query = "SELECT * FROM stg_burger_mart.dim_date"
     df = pd.read_sql(query, engine)
